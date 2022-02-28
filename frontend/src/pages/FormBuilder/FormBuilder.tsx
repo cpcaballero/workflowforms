a5fed65848eb7f81cf55d7b856cca9f87d88946d
Carlos Caballero <carlos.caballero@aspireiq.com>
2022-02-28 11:01:03 +0800

    fixed file upload field

--- frontend/src/pages/FormBuilder/FormBuilder.tsx
+++ frontend/src/pages/FormBuilder/FormBuilder.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button }  from "primereact/button";
+import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import { Divider } from 'primereact/divider';
 import { Dropdown, DropdownChangeParams } from 'primereact/dropdown';
@@ -8,17 +8,9 @@ import { InputSwitch } from 'primereact/inputswitch';
 import { RadioButton } from 'primereact/radiobutton';
 import { Checkbox } from 'primereact/checkbox'
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-// import { Cloudinary } from "cloudinary";
-import { AdvancedImage, accessibility, responsive, lazyload, placeholder } from '@cloudinary/react';
 
-
-// import styles from "./FormBuilder.module.css";
-// import { PROJECT_CREATE_URL } from "../../utils/urls";
 import { useNavigate, useParams } from "react-router-dom";
-
 import useCheckLogin from "../../utils/useCheckLogin";
-
-// import axios from "axios";
 import { v4 as uuidv4 } from 'uuid';
 
 interface iFormItem {
@@ -39,53 +31,51 @@ interface iFormData {
 interface iUpdateField {
   value?: any,
   uuid: string,
-  field:string,
-  choiceIndex?:number,
-  checked?:boolean,
+  field: string,
+  choiceIndex?: number,
+  checked?: boolean,
 }
 
 const fieldTypes = [
-  {name: "Short text", value: "TEXT"},
-  {name: "Long text", value: "LONG_TEXT"},
-  {name: "Yes/No Question", value: "YES_NO"},
-  {name: "Single Choice", value: "SINGLE_SELECT"},
-  {name: "Multiple Choice", value: "MULTIPLE_SELECT"},
-  {name: "Date", value: "DATE"},
-  {name: "Image", value: "IMAGE"}
+  { name: "Short text", value: "TEXT" },
+  { name: "Long text", value: "LONG_TEXT" },
+  { name: "Yes/No Question", value: "YES_NO" },
+  { name: "Single Choice", value: "SINGLE_SELECT" },
+  { name: "Multiple Choice", value: "MULTIPLE_SELECT" },
+  { name: "Date", value: "DATE" },
+  { name: "Image", value: "IMAGE" }
 
 ];
 
-
 export const FormBuilder: React.FunctionComponent = () => {
   const user = useCheckLogin();
-  const {formId} = useParams();
+  const { formId } = useParams();
   const navigate = useNavigate();
-  const {useState, useEffect, useMemo} = React;
+  const { useState, useEffect, useMemo } = React;
   const droppableId = useMemo(() => uuidv4(), []);
   const [formData, setFormData] = useState<iFormData>({
     formTitle: "",
     formItems: []
   });
-
   const [profile, setProfile] = useState<any>();
 
-  const handleOnChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     return setFormData({
       ...formData,
-      [e.target.name]:e.target.value
+      [e.target.name]: e.target.value
     });
   };
 
-  useEffect( () => {
-    if(user !== undefined){
+  useEffect(() => {
+    if (user !== undefined) {
       setProfile(user);
     }
   }, [user]);
 
-  const addField = (e : DropdownChangeParams) => {
-    const {value} = e;
+  const addField = (e: DropdownChangeParams) => {
+    const { value } = e;
     let tempFormItems = [...formData.formItems];
-    const tempFormItem:iFormItem = {
+    const tempFormItem: iFormItem = {
       text: "",
       subtext: "",
       schema: value,
@@ -95,7 +85,7 @@ export const FormBuilder: React.FunctionComponent = () => {
       acceptTypes: [],
     }
     tempFormItems.push(tempFormItem);
-    setFormData({...formData, formItems: tempFormItems});
+    setFormData({ ...formData, formItems: tempFormItems });
   }
 
   const addFieldDropdown = (
@@ -111,43 +101,51 @@ export const FormBuilder: React.FunctionComponent = () => {
   );
 
   const updateField = (props: iUpdateField) => {
-    const { value, uuid, field, choiceIndex} = props;
+    const { value, uuid, field, choiceIndex, checked } = props;
     let currentFormItems = [...formData.formItems];
     const currentIndex = currentFormItems.findIndex(
       formItem => formItem.uuid === uuid
     );
-    switch(field){
+    switch (field) {
       case "required":
         currentFormItems[currentIndex].required = value;
-      break;
+        break;
       case "text":
         currentFormItems[currentIndex].text = value;
-      break;
+        break;
       case "subtext":
         currentFormItems[currentIndex].subtext = value;
-      break;
+        break;
       case "choice_add":
         currentFormItems[currentIndex].choices?.push("");
-      break;
+        break;
       case "choice_update":
-        if(choiceIndex !== undefined){
+        if (choiceIndex !== undefined) {
           currentFormItems[currentIndex].choices[choiceIndex] = value;
         }
-      break;
+        break;
       case "choice_delete":
         currentFormItems[currentIndex].choices = currentFormItems[currentIndex]
           .choices
           .filter((choice, index) => (
             index !== choiceIndex
           ));
-      break;
+        break;
       case "field_remove":
         currentFormItems = formData.formItems.filter(
           formItem => formItem.uuid !== uuid
         );
+        break;
       case "file_type":
-
-      break;
+        let acceptTypes = [...currentFormItems[currentIndex].acceptTypes];
+        if (checked) {
+          acceptTypes.push(value);
+        } else {
+          acceptTypes = acceptTypes.filter(type => type !== value);
+        }
+        currentFormItems[currentIndex].acceptTypes = Array.from(new Set(acceptTypes));
+        console.log(currentFormItems[currentIndex].acceptTypes);
+        break;
     }
     setFormData({
       ...formData,
@@ -156,13 +154,16 @@ export const FormBuilder: React.FunctionComponent = () => {
   }
 
   const renderField = (formItem: iFormItem) => {
-    switch(formItem.schema){
+    switch (formItem.schema) {
       case "TEXT":
       case "LONG_TEXT":
       case "YES_NO":
       case "DATE":
-        return(
-          <div className="p-grid">
+        return (
+          <div
+            className="p-grid"
+            key={"wrapper-" + formItem.uuid}
+          >
             <div className="p-d-flex p-flex-column p-col-10">
               <InputText
                 autoFocus
@@ -204,8 +205,11 @@ export const FormBuilder: React.FunctionComponent = () => {
         );
       case "SINGLE_SELECT":
       case "MULTIPLE_SELECT":
-        return(
-          <div className="p-grid">
+        return (
+          <div
+            className="p-grid"
+            key={"wrapper-" + formItem.uuid}
+          >
             <div className="p-d-flex p-flex-column p-col-10">
               <InputText
                 autoFocus
@@ -239,7 +243,7 @@ export const FormBuilder: React.FunctionComponent = () => {
               />
               <ul>
                 {
-                  formItem.choices?.map( (choice, index) => (
+                  formItem.choices?.map((choice, index) => (
                     <li className="p-d-flex p-flex-row p-ai-center">
                       <RadioButton
                         checked={false}
@@ -283,8 +287,29 @@ export const FormBuilder: React.FunctionComponent = () => {
           </div>
         );
       case "IMAGE":
-        return(
-          <div className="p-grid">
+        const fileTypes = [
+          {
+            value: "images/*",
+            label: "Images (.jpeg, .jpg, .gif, or .png file extensions)",
+          },
+          {
+            value: ".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            label: "Word Documents (.doc, or .docx file extensions)"
+          },
+          {
+            value: ".pdf, ",
+            label: "PDF File (.pdf file extensions)"
+          },
+          {
+            value: ".xls, .xlsx",
+            label: "Excel file (.xls, .xlsx file extensions)"
+          },
+        ];
+        return (
+          <div
+            className="p-grid"
+            key={"wrapper-" + formItem.uuid}
+          >
             <div className="p-d-flex p-flex-column p-col-10">
               <InputText
                 autoFocus
@@ -308,38 +333,35 @@ export const FormBuilder: React.FunctionComponent = () => {
                 value={formItem.subtext}
                 placeholder="Optional subtext here"
               />
-              <Button
-                  className="pi pi-plus p-button-secondary p-col-2"
-                  label="Add option"
-                  onClick={(e) => updateField({
-                    uuid: formItem.uuid,
-                    field: "choice_add",
-                  })}
-                />
-                <ul>
-                  <li>
-                    <div className="col-12">
+              <h5>Filetypes to accept (choose one or more)</h5>
+              <ul>
+                {
+                  fileTypes.map(type => (
+                    <li className="p-d-flex p-flex-row p-ai-center">
                       <Checkbox
-                        inputId={"cb-img-"+ formItem.uuid}
-                        value="image/*"
+                        className="p-mr-2"
+                        inputId={"cb-img-" + formItem.uuid}
+                        value={type.value}
                         onChange={(e) => updateField({
                           value: e.value,
                           uuid: formItem.uuid,
                           field: "file_type",
                           checked: e.checked,
                         })}
-                        checked={formItem.acceptTypes.includes("image/*")}
+                        checked={formItem.acceptTypes.includes(type.value)}
                       >
                       </Checkbox>
                       <label
-                        htmlFor={"cb-img-"+ formItem.uuid}
+                        htmlFor={"cb-img-" + formItem.uuid}
                         className="p-checkbox-label"
                       >
-                        Images (.jpeg, .jpg, .gif, .png)
+                        {type.label}
                       </label>
-                    </div>
-                  </li>
-                </ul>
+                    </li>
+                  ))
+                }
+
+              </ul>
             </div>
             <div className="p-col-2 p-d-flex p-ai-center">
               <InputSwitch
@@ -358,10 +380,15 @@ export const FormBuilder: React.FunctionComponent = () => {
     }
   }
 
-  const formHeader = (options:any, itemNumber:number, uuid:string, dragHandleProps:any) => {
-    const className=`${options.className} p-d-flex p-jc-between`;
-    const titleClassName=`${options.titleClassName}`;
-    return(
+  const formHeader = (
+    options: any,
+    itemNumber: number,
+    uuid: string,
+    dragHandleProps: any
+  ) => {
+    const className = `${options.className} p-d-flex p-jc-between`;
+    const titleClassName = `${options.titleClassName}`;
+    return (
       <div
         className={className}
       >
@@ -386,19 +413,24 @@ export const FormBuilder: React.FunctionComponent = () => {
     )
   }
 
-  return(
+  return (
     <div className={"p-pt-6 p-px-6"}>
       <div className="p-d-flex p-flex-column p-jc-start">
         <div className="p-d-flex p-flex-row p-jc-between p-grid">
-            <div className="p-d-flex p-flex-row p-sm-10 p-md-8 p-lg-6">
-              <h1 className="p-mb-0 p-mr-2">Form Builder:</h1>
-              <InputText
-                className="p-col"
-                value={formData.formTitle}
-                onChange={(e) => setFormData({...formData, formTitle: e.target.value})}
-                placeholder="Enter your form title here"
-              />
-            </div>
+          <div className="p-d-flex p-flex-row p-sm-10 p-md-8 p-lg-6">
+            <h1 className="p-mb-0 p-mr-2">Form Builder:</h1>
+            <InputText
+              className="p-col"
+              value={formData.formTitle}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  formTitle: e.target.value
+                })
+              }
+              placeholder="Enter your form title here"
+            />
+          </div>
           <div className="p-d-flex p-ai-center">
             <Button
               className="p-mx-2"
@@ -417,7 +449,7 @@ export const FormBuilder: React.FunctionComponent = () => {
             const sourceIndex = param.source.index;
             const destIndex = param.destination?.index;
             const [removedItem] = currentFormItems.splice(sourceIndex, 1);
-            if(destIndex !== undefined ){
+            if (destIndex !== undefined) {
               currentFormItems.splice(destIndex, 0, removedItem);
               setFormData({
                 ...formData,
@@ -428,7 +460,7 @@ export const FormBuilder: React.FunctionComponent = () => {
         >
           <div className="p-sm-12 p-md-8 p-as-center">
             <Droppable droppableId={droppableId}>
-              {(provided, _ ) => (
+              {(provided, _) => (
                 <div
                   ref={provided.innerRef}
                   {...provided.droppableProps}
@@ -438,7 +470,7 @@ export const FormBuilder: React.FunctionComponent = () => {
                       (formItem, index) => (
                         <Draggable
                           key={formItem.uuid}
-                          draggableId={"draggable-"+formItem.uuid}
+                          draggableId={"draggable-" + formItem.uuid}
                           index={index}
                         >
                           {(provided, snapshot) => (
@@ -455,7 +487,7 @@ export const FormBuilder: React.FunctionComponent = () => {
                               <Panel
                                 className="p-my-3"
                                 headerTemplate={(
-                                  options) => formHeader(options, index+1, formItem.uuid, {...provided.dragHandleProps})
+                                  options) => formHeader(options, index + 1, formItem.uuid, { ...provided.dragHandleProps })
                                 }
                               >
                                 {renderField(formItem)}
